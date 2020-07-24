999367e1c46e7d95b289848564d1aadb0c674a67
mcelep <muratcelep@yahoo.com>
2020-07-24 16:03:49 +0200

    Add Spring Initializr artifacts

---
+++ src/test/java/com/example/doodleapi/DoodleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.doodleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoodleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
