ba2e697deef4ce59475c732f6ac8ee091a8ebe18
mcelep <muratcelep@yahoo.com>
2020-07-25 22:07:41 +0200

    Add test cases for titles with non-ASCII chars

--- src/test/java/com/example/doodle/api/ApiControllerTest.java
+++ src/test/java/com/example/doodle/api/ApiControllerTest.java
@@ -13,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URLEncoder;
+import java.util.Arrays;
 import java.util.List;
 
 import static java.net.URLEncoder.encode;
@@ -31,7 +32,7 @@ public class ApiControllerTest {
     void findsPollsByUser(){
         // When
         final ResponseEntity<List<Poll>> response = restCall("/polls/email?query=", "mh+sample@doodle.com");
-        // Expect
+        // Then
         isTrue(response.getStatusCode() == HttpStatus.OK, "Return code must be 200 OK, when doing a get");
         isTrue(response.getBody().size() == 36);
     }
@@ -40,19 +41,33 @@ public class ApiControllerTest {
     void findsPollsByTitle() {
         // When
         final ResponseEntity<List<Poll>> response = restCall("/polls/title?query=", "Qui sont les superhéros Marvel les plus oufs?");
-        // Expect
+        // Then
         isTrue(response.getStatusCode() == HttpStatus.OK, "Return code must be 200 OK, when doing a get");
         isTrue(response.getBody().size() == 1);
+    }
+
+    @Test
+    void findsPollsByTitlesWithNonASCIIChars() {
+        // Given
+        List<String> titles = Arrays.asList("ليتل جاي يتحول اثنين!",
+                                            "小杰伊两岁！",
+                                            "谁是最坏蛋奇迹超级英雄？");
+        // When
+        for (String title:titles) {
+            ResponseEntity<List<Poll>> response = restCall("/polls/title?query=", title);
+            isTrue(response.getStatusCode() == HttpStatus.OK, "Return code must be 200 OK, when doing a get");
+            isTrue(response.getBody().size() == 1,String.format("For title:'%s', there should be exactly 1 match.",title));
+            isTrue(response.getBody().get(0).title.contentEquals(title),String.format("For title:'%s', what's returned from server should match exact.",title));
+        }
     }
 
     @Test
     void findsPollsByCreationDate(){
         // When
         final ResponseEntity<List<Poll>> response = restCall("/polls/created-after?query=","1485477127056");
-        // Expect
+        // Then
         isTrue(response.getStatusCode() == HttpStatus.OK, "Return code must be 200 OK, when doing a get");
         isTrue(response.getBody().size() == 1);
-
     }
 
     private ResponseEntity<List<Poll>> restCall(String path, String query)  {
