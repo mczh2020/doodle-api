79e2a3fe1ac8be821c123b580523fd40e1d7f67f
mcelep <muratcelep@yahoo.com>
2020-07-26 16:26:03 +0200

    Refactoring: Remove JsonPropery annotatoions, they are needed as java field names are same as the value of json attribute.

--- src/main/java/com/example/doodle/api/model/Location.java
+++ src/main/java/com/example/doodle/api/model/Location.java
@@ -1,16 +1,11 @@
 package com.example.doodle.api.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Location {
-    @JsonProperty("name")
     public String name;
-    @JsonProperty("address")
     public String address;
-    @JsonProperty("countryCode")
     public String countryCode;
-    @JsonProperty("locationId")
     public String locationId;
 }
