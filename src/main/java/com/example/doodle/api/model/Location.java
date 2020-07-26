de13c04f8e9f0505ca9e54074c110d61fb758809
mcelep <muratcelep@yahoo.com>
2020-07-26 13:01:30 +0200

    Refactoring: Simplify location model, JsonProperty is not necessarily needed at this point

--- src/main/java/com/example/doodle/api/model/Location.java
+++ src/main/java/com/example/doodle/api/model/Location.java
@@ -2,15 +2,8 @@ package com.example.doodle.api.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonPropertyOrder({
-        "name",
-        "address",
-        "countryCode",
-        "locationId"
-})
 public class Location {
     @JsonProperty("name")
     public String name;
